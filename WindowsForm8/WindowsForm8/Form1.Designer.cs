9b7a0cc8554f6788beae868375913e9304f1a1d7
gamzegungor1 <102431618+gamzegungor1@users.noreply.github.com>
2022-04-22 21:59:02 +0300

    Add files via upload

---
+++ WindowsForm8/WindowsForm8/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace WindowsForm8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_kulAd = new System.Windows.Forms.Label();
+            this.txt_kulad = new System.Windows.Forms.TextBox();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.lbl_sifre = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsForm8.Properties.Resources.Ekran_görüntüsü_2022_04_22_212413;
+            this.pictureBox1.Location = new System.Drawing.Point(66, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_kulAd
+            // 
+            this.lbl_kulAd.AutoSize = true;
+            this.lbl_kulAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_kulAd.Location = new System.Drawing.Point(341, 61);
+            this.lbl_kulAd.Name = "lbl_kulAd";
+            this.lbl_kulAd.Size = new System.Drawing.Size(130, 25);
+            this.lbl_kulAd.TabIndex = 1;
+            this.lbl_kulAd.Text = "Kullanıcı Adı: ";
+            this.lbl_kulAd.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_kulad
+            // 
+            this.txt_kulad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_kulad.Location = new System.Drawing.Point(503, 65);
+            this.txt_kulad.Name = "txt_kulad";
+            this.txt_kulad.Size = new System.Drawing.Size(100, 26);
+            this.txt_kulad.TabIndex = 0;
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_giris.Location = new System.Drawing.Point(346, 179);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(257, 36);
+            this.btn_giris.TabIndex = 2;
+            this.btn_giris.Text = "Giriş Yap : ";
+            this.btn_giris.UseVisualStyleBackColor = true;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_sifre.Location = new System.Drawing.Point(503, 129);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(100, 26);
+            this.txt_sifre.TabIndex = 1;
+            // 
+            // lbl_sifre
+            // 
+            this.lbl_sifre.AutoSize = true;
+            this.lbl_sifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sifre.Location = new System.Drawing.Point(341, 128);
+            this.lbl_sifre.Name = "lbl_sifre";
+            this.lbl_sifre.Size = new System.Drawing.Size(68, 25);
+            this.lbl_sifre.TabIndex = 4;
+            this.lbl_sifre.Text = "Şifre : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 451);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.lbl_sifre);
+            this.Controls.Add(this.btn_giris);
+            this.Controls.Add(this.txt_kulad);
+            this.Controls.Add(this.lbl_kulAd);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_kulAd;
+        private System.Windows.Forms.TextBox txt_kulad;
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Label lbl_sifre;
+    }
+}
+
---
+++ WindowsForm8/WindowsForm8/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_giris_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi, sifre = "";
+            sifre = txt_sifre.Text;
+            kullaniciAdi = txt_kulad.Text;
+            if(kullaniciAdi.ToLower()=="gamze" && sifre=="1")
+            {
+                MessageBox.Show("Başarılı Bir Şekilde Giriş Yaptınız. ", "Bilgilendirme");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış Kullanıcı Adı veya Şifre", "Bilgilendirme");
+            }
+
+        }
+    }
+}
